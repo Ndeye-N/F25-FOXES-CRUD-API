8ef52b11a849ec77d37922fb24d24360ce0751c2
Ndeye-N <nmndiaye@uncg.edu>
2025-10-09 00:00:04 -0400

    Scaffold Spring Boot project (Web, JPA, Postgres) + .gitignore

---
+++ src/main/java/com/ndeye/foxes_api/FoxesApiApplication.java
@@ -0,0 +1,13 @@
+package com.ndeye.foxes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoxesApiApplication.class, args);
+	}
+
+}
